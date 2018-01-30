f7dba41a40cb3abc2a824ca6242e27240d90e872
leizhiheng <leizhiheng@cwtcn.com>
2018-01-30 09:26:51 +0800

    修改NoDisturbMode.java,增加免打扰模式的功能代码

--- src/com/cwtcn/leshanandroidlib/resources/NoDisturbMode.java
+++ src/com/cwtcn/leshanandroidlib/resources/NoDisturbMode.java
@@ -1,10 +1,23 @@
 package com.cwtcn.leshanandroidlib.resources;
 
+import android.app.NotificationManager;
+import android.content.ComponentName;
+import android.net.Uri;
+import android.provider.Settings;
+import android.service.notification.ZenModeConfig;
+import android.service.notification.ZenModeConfig.EventInfo;
+import android.service.notification.ZenModeConfig.ScheduleInfo;
+import android.service.notification.ZenModeConfig.ZenRule;
+
 import com.cwtcn.leshanandroidlib.utils.DebugLog;
 
 import org.eclipse.leshan.core.node.LwM2mResource;
 import org.eclipse.leshan.core.response.ReadResponse;
 import org.eclipse.leshan.core.response.WriteResponse;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * 这个类用于实现免打扰模式
@@ -25,6 +38,7 @@ public class NoDisturbMode extends ExtendBaseInstanceEnabler {
     public static final String KEY_NO_DISTURB_MODE_MSG = "NoDisturbModeMsg";
     public static final int EVENT_IDENTIFIER = 5823;
     public static final int TEXT = 5527;
+    public static final String ACTION = Settings.ACTION_ZEN_MODE_SCHEDULE_RULE_SETTINGS;
 
     @Override
     public synchronized ReadResponse read(int resourceId) {
@@ -51,14 +65,6 @@ public class NoDisturbMode extends ExtendBaseInstanceEnabler {
     }
 
     /**
-     * 根据设置Write过来的信息设置免打扰模式。
-     * @param settingMsg
-     */
-    private void setNonDisturbMode(String settingMsg) {
-        mOnWriteReadListener.setStringToPreference(KEY_NO_DISTURB_MODE_MSG, settingMsg);
-    }
-
-    /**
      * 获取免打扰模式的设置信息，以如下格式返回
      {
      "sec1": "07:00-09:30",
@@ -69,5 +75,115 @@ public class NoDisturbMode extends ExtendBaseInstanceEnabler {
      */
     private String getNoDisturbModeMsg() {
         return mOnWriteReadListener.getStringFromPreferemce(KEY_NO_DISTURB_MODE_MSG);
+    }
+
+    /**
+     * 根据设置Write过来的信息设置免打扰模式。
+     * @param settingMsg
+     */
+    public void setNonDisturbMode(String settingMsg) {
+        DebugLog.d("setNonDisturbMode == >");
+        settingMsg = "{\"sec1\": \"07:00-09:30\",\"sec2\": \"10:00-13:40\",\"repeatExpression\": \"0111110\"}";
+        //mOnWriteReadListener.setStringToPreference(KEY_NO_DISTURB_MODE_MSG, settingMsg);
+        setZenModeConfig(getZenModeConfig(settingMsg));
+    }
+
+
+    protected boolean setZenModeConfig(ZenModeConfig config) {
+
+        final String reason = getClass().getSimpleName();
+        final boolean success = NotificationManager.from(mContext).setZenModeConfig(config, reason);
+        if (success) {
+            DebugLog.d("Saved mConfig=" + config);
+        }
+        return success;
+    }
+
+    private ZenModeConfig getZenModeConfig(String settingMsg) {
+        DebugLog.d("getZenModeConfig == >");
+        try {
+            JSONObject jsonObject = new JSONObject(settingMsg);
+            String sec1 = jsonObject.getString("sec1");
+            String sec2 = jsonObject.getString("sec2");
+            String repeatExpression = jsonObject.getString("repeatExpression");
+
+            RuleInfo info1 = getRuleInfo(sec1, repeatExpression);
+            RuleInfo info2 = getRuleInfo(sec2, repeatExpression);
+
+            ZenRule rule1 = getZenRule(info1, "sec1");
+            ZenRule rule2 = getZenRule(info2, "sec2");
+
+            final ZenModeConfig newConfig = new ZenModeConfig();
+            final String ruleId1 = newConfig.newRuleId();
+            final String ruleId2 = newConfig.newRuleId();
+            newConfig.automaticRules.put(ruleId1, rule1);
+            newConfig.automaticRules.put(ruleId2, rule2);
+
+            return newConfig;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            DebugLog.d("getZenModeConfig failed, e:" + e.getMessage());
+        }
+
+        return null;
+    }
+
+    private ZenRule getZenRule(RuleInfo ri, String name) {
+        final ZenRule rule = new ZenRule();
+        rule.name = name;
+        rule.enabled = true;
+        rule.zenMode = Settings.Global.ZEN_MODE_ALARMS;
+        rule.conditionId = ri.defaultConditionId;
+        rule.component = null;
+        DebugLog.d("getZenRule ZenRule:" + rule);
+
+        return rule;
+    }
+
+    /**
+     *
+     * @param time 防干扰的一个时间段，格式："07:00-09:30"
+     * @param repeat 一周中的重复日期，格式："0111110"， "0"表示关闭；"1"表示开启
+     * @return
+     */
+    private RuleInfo getRuleInfo(String time, String repeat) {
+        String times[] = time.split("-");
+        String startTimeParts[] = times[0].split(":");
+        String endTimeParts[] = times[1].split(":");
+
+        final ScheduleInfo schedule = new ScheduleInfo();
+        schedule.days = getRepeatWeekDay(repeat);//ZenModeConfig.ALL_DAYS;
+        schedule.startHour = Integer.valueOf(startTimeParts[0]);
+        schedule.startMinute = Integer.valueOf(startTimeParts[1]);
+        schedule.endHour = Integer.valueOf(endTimeParts[0]);
+        schedule.endMinute = Integer.valueOf(endTimeParts[1]);
+        final RuleInfo rt = new RuleInfo();
+        rt.settingsAction = ACTION;
+        rt.defaultConditionId = ZenModeConfig.toScheduleConditionId(schedule);
+        DebugLog.d("getRuleInfo ruluInfor:" + rt);
+
+        return rt;
+    }
+
+    private int[] getRepeatWeekDay(String repeat) {
+        char [] rChars = repeat.toCharArray();
+        int [] allDay = ZenModeConfig.ALL_DAYS;
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        for (int i = 0; i < allDay.length; i++) {
+            if (rChars[i] == 1) {
+                temp.add(i);
+            }
+        }
+        int [] days = new int[temp.size()];
+        for (int i = 0; i < temp.size(); i++) {
+            days[i] = allDay[i];
+        }
+        return days;
+    }
+
+    public static class RuleInfo {
+        public String caption;
+        public String settingsAction;
+        public Uri defaultConditionId;
     }
 }
